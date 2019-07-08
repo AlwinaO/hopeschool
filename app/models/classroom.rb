3909da917d373f707493295084be3b8388678f88
AlwinaO <aoyewole2586@gmail.com>
2019-07-07 21:28:29 -0400

    add validations to model

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,4 +2,11 @@ class Classroom < ApplicationRecord
   belongs_to :teacher
   belongs_to :student
   belongs_to :semester
+
+  validates :name, :location, presence: true
+  validates :name, :location, uniqueness: true
+
+  # list the teacher, number of students and the current semester
+
+  
 end

17183b95e3541b4b765ef269eeef2ca4e981bdb3
AlwinaO <aoyewole2586@gmail.com>
2019-07-08 22:46:14 -0400

    update with Factory.create to create a valid factory

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Classroom, :type => :model do
 
+  it "has a valid factory" do
+    Factory.create(:classroom).should be_valid
+  end
+
   # ensure this join table is setup with user-submittable attributes
   context "columns" do
     it { should have_db_column(:name).of_type(:string)}
@@ -26,7 +30,7 @@ RSpec.describe Classroom, :type => :model do
 
   context "#classroom_list" do
     it "lists all the classrooms by name and location" do
-      
+
 
     end
   end

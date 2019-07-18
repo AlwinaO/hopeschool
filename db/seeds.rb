876ec09542374f3540b661c6533db38e8b30d875
AlwinaO <aoyewole2586@gmail.com>
2019-07-17 22:19:25 -0400

    build the seed file with Faker data; work on has_many associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+15.times do
+  Classroom.create(
+    name: Faker::Superhero.name,
+    location: Faker::Address.building_number)
+end
+
+15.times do
+  teacher = Teacher.create(
+    name:  Faker::Name.first_name,
+    email:  Faker::Internet.email,
+    password:  Faker::Internet.password,
+    subject:  Faker::Educator.subject)
+
+  teacher.classrooms.create
+end
+
+8.times do
+  Semester.create(
+    quarter:  Faker::Space.planet,
+    year: Faker::Date.between(1.year.ago, 1.year.from_now)
+  )
+end
+
+50.times do
+  Student.create(
+    name: Faker::Name.first_name,
+    grade: Faker::Number.within(1..10)
+  )
+end

391a598568985733e33c886013e59b4376852193
AlwinaO <aoyewole2586@gmail.com>
2019-07-23 22:10:17 -0400

    add has_many, through association for join table

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,9 +1,10 @@
 class Teacher < ApplicationRecord
   has_secure_password
 
-  has_one :classrooms
+  has_one :classroom
   has_many :students
-  has_many :semesters
+  has_many :teacher_semesters
+  has_many :semesters, through: :teacher_semesters
 
   validates :name, :email, :subject, presence: true
   validates :email, uniqueness: true

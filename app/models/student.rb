723e2047f200a254171288e6e7d023aae1b06a3d
AlwinaO <aoyewole2586@gmail.com>
2019-07-23 22:10:28 -0400

    add has_many, through association for join table

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 class Student < ApplicationRecord
-  belongs_to :classrooms
+  belongs_to :classroom
   belongs_to :teacher
-  has_many :semesters
+  has_many :student_semesters
+  has_many :semesters, through: :student_semesters
 
   validates :name, :grade, presence: true
 end

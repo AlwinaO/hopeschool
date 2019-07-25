68fe6565185235c54a2f2ae7c627f89daf6b12d6
AlwinaO <aoyewole2586@gmail.com>
2019-07-24 22:32:13 -0400

    update seed file to associate teacher and classroom

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,39 @@ require "faker"
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
+# comment out teachers so it doesn't add 15 new teacher
 15.times do
-  teacher = Teacher.create(
+  Teacher.create(
     name:  Faker::Name.first_name,
     email:  Faker::Internet.email,
     password:  Faker::Internet.password,
     subject:  Faker::Educator.subject)
+# new_teacher.build_classroom
 
-  # teacher.classroom.create
 end
 
-8.times do
-  Semester.create(
-    quarter:  Faker::Space.planet,
-    year: Faker::Date.between(1.year.ago, 1.year.from_now)
-  )
-end
-
-50.times do
-  Student.create(
-    name: Faker::Name.first_name,
-    grade: Faker::Number.within(1..10)
-  )
-end
-
+# 8.times do
+#   Semester.create(
+#     quarter:  Faker::Space.planet,
+#     year: Faker::Date.between(1.year.ago, 1.year.from_now)
+#   )
+# end
+#
+# 50.times do
+#   Student.create(
+#     name: Faker::Name.first_name,
+#     grade: Faker::Number.within(1..10)
+#   )
+# end
+teacher_ids = Teacher.all.map {|t| t.id}
+# [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# t_id = teacher_ids.pop
+# t_id = 15
+# [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 15.times do
-  Classroom.create(
-    name: Faker::Superhero.name,
-    location: Faker::Address.building_number)
+  new_classroom = Classroom.new(
+                  name: Faker::Superhero.name,
+                  location: Faker::Address.building_number)
+  new_classroom.teacher_id = teacher_ids.pop()
+  new_classroom.save
 end

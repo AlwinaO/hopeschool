35b95b3c213e51f608109b8150f28bd2a164e034
AlwinaO <aoyewole2586@gmail.com>
2019-07-30 19:47:54 -0400

    add associations: teacher_semesters, student_semesters, classroom_semesters

--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe Semester, :type => :model do
 
   context "associations" do
-    it { should have_many(:classrooms) }
+    it { should have_many(:teacher_semesters) }
+    it { should have_many(:teachers).through(:teacher_semesters) }
+    it { should have_many(:classroom_semesters) }
+    it { should have_many(:classrooms).through(:classroom_semesters) }
+    it { should have_many(:student_semesters) }
+    it { should have_many(:students).through(:student_semesters) }
+
   end
 
   context "validations" do

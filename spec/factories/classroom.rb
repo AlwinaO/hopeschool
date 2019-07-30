1ba1b9e28cd8ab6776fb5264849ebd32748ff166
AlwinaO <aoyewole2586@gmail.com>
2019-07-30 19:45:27 -0400

    update factories to include associations

--- spec/factories/classroom.rb
+++ spec/factories/classroom.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Superhero.name}
     location { Faker::Address.building_number}
     association :teacher, factory: :teacher
-    association :student, factory: :student
-    association :semester, factory: :semester
+    # association :student, factory: :student
+    # association :semester, factory: :semester
   end
 end
--- spec/factories/student.rb
+++ spec/factories/student.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :student do
     name { Faker::Name.first_name}
     grade { Faker::Number.within(1..10)}
+    association :teacher, factory: :teacher
+    association :classroom, factory: :classroom
   end
 end

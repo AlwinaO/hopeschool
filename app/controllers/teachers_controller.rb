413f9e1f48233d6fbabacd089b689ea2a7d93860
AlwinaO <aoyewole2586@gmail.com>
2019-07-30 20:45:41 -0400

    add teacher actions to render new form, create and show teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,10 +1,28 @@
 class TeachersController < ApplicationController
 
   def new
-
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @teacher = Teacher.find_by(id: params[:id])
+
+  end
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:name, :email, :password, :subject)
 
   end
 end

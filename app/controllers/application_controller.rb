2d870bd2c0a7cc51fb5ce1d2944dca2b178de1d3
AlwinaO <aoyewole2586@gmail.com>
2019-07-30 20:46:30 -0400

    add simple authorization and authentication for teacher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+    def current_teacher
+      @current_teacher ||= Teacher.find_by(id: session[:teacher_id]) if session[:teacher_id]
+    end
+
+    def logged_in?
+      !!current_teacher
+    end
+
+    def authenticate_teacher
+      if !logged_in?
+        redirect_to root_path
+      end
+    end
 end

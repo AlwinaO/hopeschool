0721709bf6d1ededde7fc25b6fab1bfb46ad261d
AlwinaO <aoyewole2586@gmail.com>
2019-08-01 16:36:43 -0400

    add new, create actions and student_params for students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,20 @@
 class StudentsController < ApplicationController
+
+  def new
+    if params[:teacher_id] && teacher = Teacher.find_by_id(params[:teacher_id])
+      @student = teacher.students.build
+    else
+      @student = Student.new
+      @student.build_teacher
+    end
+  end
+
+  def create
+
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :grade, :score, :teacher_id)
+    end
 end

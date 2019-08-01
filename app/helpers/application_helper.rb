ef5ad3d04d9105ee23ae4a99e829ca9c5c164e39
AlwinaO <aoyewole2586@gmail.com>
2019-08-01 19:28:31 -0400

    add conditional to show different login link if user is not logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def current_teacher
+    current_teacher ||= Teacher.find_by(id: session[:teacher_id]) if session[:teacher_id]
+  end
+
+  def logged_in?
+    !!current_teacher
+  end
+
+  def authenticate_teacher
+    if !logged_in?
+      redirect_to root_path
+    end
+  end
 end

e700c5d8a05a6faa6e3e910bc727bc744644cbd2
AlwinaO <aoyewole2586@gmail.com>
2019-08-02 17:49:06 -0400

    add comments to new action and set up create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,29 @@
 class StudentsController < ApplicationController
 
   def new
+    # check if the student is nested and it has the proper teacher id
     if params[:teacher_id] && teacher = Teacher.find_by_id(params[:teacher_id])
-      @student = teacher.students.build
+      @student = teacher.students.build #has_many
     else
       @student = Student.new
-      @student.build_teacher
+      @student.build_teacher #belongs_to
     end
   end
 
   def create
+    @student = current_teacher.students.build(student_params)
+    if @student.save
+      redirect_to teacher_student_path(@student)
+    else
+      @student.build_teacher unless @student.teacher
+      render :new
+    end
+  end
+
+  def index
+    if params[:teacher_id] && teacher = Teacher.find_by_id(params[:teacher_id])
+      @students = teacher.students
+    end 
 
   end
 

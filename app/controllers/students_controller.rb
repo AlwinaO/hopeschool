3563071a19efebaa9e752718d995a210317ce7c5
AlwinaO <aoyewole2586@gmail.com>
2019-08-02 23:06:05 -0400

    add remaining methods to controller for restful routing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,9 +11,10 @@ class StudentsController < ApplicationController
   end
 
   def create
+    current_teacher = Teacher.find_by_id(params[:teacher_id])
     @student = current_teacher.students.build(student_params)
     if @student.save
-      redirect_to teacher_student_path(@student)
+      redirect_to teacher_student_path(current_teacher, @student)
     else
       @student.build_teacher unless @student.teacher
       render :new
@@ -21,14 +22,39 @@ class StudentsController < ApplicationController
   end
 
   def index
-    if params[:teacher_id] && teacher = Teacher.find_by_id(params[:teacher_id])
-      @students = teacher.students
-    end 
+    @students = Student.all
 
+  end
+
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  # create a private method to set the student by finding the current_teacher and student for the edit, update,  and destroy methods
+  def edit
+    current_teacher = Teacher.find_by_id(params[:teacher_id])
+    @student = Student.find_by_id(params[:id])
+  end
+
+  def update
+    current_teacher = Teacher.find_by_id(params[:teacher_id])
+    @student = Student.find_by_id(params[:id])
+    if @student.update(student_params)
+      redirect_to teacher_student_path(current_teacher, @student)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    current_teacher = Teacher.find_by_id(params[:teacher_id])
+    @student = Student.find_by_id(params[:id])
+    @student.destroy
+    redirect_to teacher_students_path
   end
 
   private
     def student_params
-      params.require(:student).permit(:name, :grade, :score, :teacher_id)
+      params.require(:student).permit(:name, :grade, :score, :teacher_id, :classroom_id)
     end
 end

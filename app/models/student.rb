948a441676c43e941099ec7e444e530fb624a61f
AlwinaO <aoyewole2586@gmail.com>
2019-08-03 21:05:20 -0400

    add dependent destroy to student_semesters association & instance methods

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,17 @@
 class Student < ApplicationRecord
   belongs_to :classroom, optional: true
   belongs_to :teacher
-  has_many :student_semesters
+  has_many :student_semesters, dependent: :destroy
   has_many :semesters, through: :student_semesters
 
   validates :name, :grade, presence: true
+
+  def classroom_name
+    self.classroom ? self.classroom.name : nil
+  end
+
+  def teacher_name
+    self.teacher ? self.teacher.name : nil 
+
+  end
 end

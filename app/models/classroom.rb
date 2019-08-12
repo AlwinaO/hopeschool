e4f2e6a977643dd4cfde70e11252612ca2c66de9
AlwinaO <aoyewole2586@gmail.com>
2019-08-12 17:12:48 -0400

    add optional: true to belongs_to association

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,12 +1,12 @@
 class Classroom < ApplicationRecord
-  belongs_to :teacher
+  belongs_to :teacher, optional: true
   has_many :students
   has_many :classroom_semesters
   has_many :semesters, through: :classroom_semesters
 
   validates :name, :location, presence: true
   validates :name, :location, uniqueness: true
-  
+
 
 
   # list the teacher, number of students and the current semester

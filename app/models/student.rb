86fc484dbd5fbdcbf09b8781b203b74bdb256ead
AlwinaO <aoyewole2586@gmail.com>
2019-08-12 20:41:20 -0400

    wip - adding classroom to student forms

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,17 +1,19 @@
 class Student < ApplicationRecord
-  belongs_to :classroom, optional: true
+  belongs_to :classroom
   belongs_to :teacher
   has_many :student_semesters, dependent: :destroy
   has_many :semesters, through: :student_semesters
 
   validates :name, :grade, presence: true
 
+  # accepts_nested_attributes_for :classroom, reject_if: :all_blank
+
   def classroom_name
     self.classroom ? self.classroom.name : nil
   end
 
   def teacher_name
-    self.teacher ? self.teacher.name : nil 
+    self.teacher ? self.teacher.name : nil
 
   end
 end

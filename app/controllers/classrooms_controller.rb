241ba4f0a94f1ea56903db3bebf4811e2109d1d4
AlwinaO <aoyewole2586@gmail.com>
2019-08-12 20:52:21 -0400

    update classrooms controller with index and show actions

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,2 +1,10 @@
 class ClassroomsController < ApplicationController
+
+  def index
+    @classrooms = Classroom.all
+  end
+
+  def show
+    @classroom = Classroom.find_by(id: params[:id])
+  end
 end

cbe10da9e9178ef05133de2be9805f14cbbdf929
AlwinaO <aoyewole2586@gmail.com>
2019-08-13 23:31:44 -0400

    remove extra code and line spacing

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -11,20 +11,11 @@ class Teacher < ApplicationRecord
 
   accepts_nested_attributes_for :classroom, reject_if: :all_blank
 
-  # def classroom_attributes=(classroom_attributes)
-  #   classroom_attributes.each do |i, classroom_attribute|
-  #     if classroom_attributes[:name].present?
-  #       classroom = Classroom.find_or_create_by(name:  classroom_attributes[:name])
-  #       if !self.classroom.include?(classroom)
-  #         self.build_classroom(:classroom => classroom)
-  #       end
-  #     end
-  #   end
-  # end
-
   def classroom_name
     self.classroom ? self.classroom.name : nil
   end
 
   #add scope method to find students with the highest or lowest score
+
+
 end

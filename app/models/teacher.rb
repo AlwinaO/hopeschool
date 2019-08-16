dbe81df8a5e3494ea2b2a4f586744701fba3b9f2
AlwinaO <aoyewole2586@gmail.com>
2019-08-15 20:59:05 -0400

    work in progress to add teacher_semesters attributes

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,13 +5,14 @@ class Teacher < ApplicationRecord
   has_many :students
   has_many :highest_score_students, -> { highest_scores }, class_name: "Student"
   has_many :lowest_score_students, -> { lowest_scores }, class_name: "Student"
-  has_many :teacher_semesters
+  has_many :teacher_semesters, inverse_of: :teacher
   has_many :semesters, through: :teacher_semesters
 
   validates :name, :email, :subject, presence: true
   validates :email, uniqueness: true
 
   accepts_nested_attributes_for :classroom, reject_if: :all_blank
+  accepts_nested_attributes_for :teacher_semesters, allow_destroy: true, reject_if: :all_blank
 
   def classroom_name
     self.classroom ? self.classroom.name : nil
@@ -22,5 +23,11 @@ class Teacher < ApplicationRecord
   # end
   #add scope method to find students with the highest or lowest score
 
-
+  # def semesters_attributes=(semester_attributes)
+  #   (semester_attributes)
+  #   semester_attributes.values.each do |semester_attribute|
+  #     semester = Semester.find_or_create_by(semester_attribute)
+  #     self.semesters << semester
+  #   end
+  # end
 end

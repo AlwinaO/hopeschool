3027a7451d0b07c7a35e505c9cec41fb6d1ac468
AlwinaO <aoyewole2586@gmail.com>
2019-08-16 22:54:45 -0400

    update semseter controller with index, new, create, edit, update

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,2 +1,38 @@
 class SemestersController < ApplicationController
+
+  def index
+    @semesters = Semester.all
+  end
+
+  def new
+    @semester = Semester.new
+  end
+
+  def create
+    @semester = Semester.new(semester_params)
+    if @semester.save
+      redirect_to semesters_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @semester = Semester.find_by(id: params[:id])
+  end
+
+  def update
+    @semester = Semester.find_by(id: params[:id])
+    if @semester.update(semester_params)
+      redirect_to semesters_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def semester_params
+      params.require(:semester).permit(:quarter, :year)
+    end
 end

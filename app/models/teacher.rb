ffbf3c632b7189b71cf26ff2b44234ca462a1853
AlwinaO <aoyewole2586@gmail.com>
2019-08-19 22:03:47 -0400

    remove commented/not useful code; wip grade_format method

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -12,22 +12,14 @@ class Teacher < ApplicationRecord
   validates :email, uniqueness: true
 
   accepts_nested_attributes_for :classroom, reject_if: :all_blank
-  accepts_nested_attributes_for :teacher_semesters, allow_destroy: true, reject_if: :all_blank
+  # accepts_nested_attributes_for :semesters
+  # accepts_nested_attributes_for :teacher_semesters, allow_destroy: true, reject_if: lambda {|attributes| attributes['teacher_grade'].blank?}
 
   def classroom_name
     self.classroom ? self.classroom.name : nil
   end
 
-  # def classroom_name=(roomname)
-  #   self.classroom = Classroom.create(name: roomname)
-  # end
-  #add scope method to find students with the highest or lowest score
-
-  # def semesters_attributes=(semester_attributes)
-  #   (semester_attributes)
-  #   semester_attributes.values.each do |semester_attribute|
-  #     semester = Semester.find_or_create_by(semester_attribute)
-  #     self.semesters << semester
-  #   end
+  # def grade_format
+  #   self.teacher_semesters ? self.teacher_semesters.collect { |ts| ts.teacher_grade.to_i} : nil
   # end
 end

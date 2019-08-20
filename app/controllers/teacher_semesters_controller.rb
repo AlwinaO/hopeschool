80a1347d40754f309b2a5965d494fd0f1fae28fd
AlwinaO <aoyewole2586@gmail.com>
2019-08-19 21:58:51 -0400

    update new, create actions and strong params for form

--- app/controllers/teacher_semesters_controller.rb
+++ app/controllers/teacher_semesters_controller.rb
@@ -1,13 +1,19 @@
 class TeacherSemestersController < ApplicationController
 
   def new
-    @teacher_semester = TeacherSemester.new
+    current_teacher = Teacher.find_by(id: params[:teacher_id])
+    if (params[:teacher_id] && current_teacher)
+      @teacher_semester = current_teacher.teacher_semesters.build
+    else
+      @teacher_semester = TeacherSemester.new
+      @teacher_semester.build_teacher
+    end
+
   end
 
   def create
-    @teacher_semester = TeacherSemester.new
-    current_teacher = Teacher.find_by(id: params[:teacher_id])
-    binding.pry
+    @teacher_semester = current_teacher.teacher_semesters.build(teacher_semester_params)
+    # binding.pry
     if @teacher_semester.save
       redirect_to teacher_path(current_teacher)
     else
@@ -18,7 +24,7 @@ class TeacherSemestersController < ApplicationController
   private
 
     def teacher_semester_params
-      params.require(teacher_semester).permit(:teacher_grade, :semester_id)
+      params.require(:teacher_semester).permit(:teacher_grade, :teacher_id, :semester_id)
     end
 
 end

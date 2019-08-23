8336061864d6f691a1050831f4cfde89e5c6c620
AlwinaO <aoyewole2586@gmail.com>
2019-08-22 22:09:10 -0400

    update create action to find a teacher; add omniauth action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,18 +11,25 @@ class SessionsController < ApplicationController
 
   # login the teacher if already signed up
   def create
-    # binding.pry
-    if request.env['omniauth.auth']
-      teacher = Teacher.create_with_omniauth(auth)
-      session[:teacher_id] = teacher.id
-      redirect_to teacher_path(teacher.id)
-    elsif teacher = Teacher.find_by_email(params[:email])
-      teacher && teacher.authenticate(params[:password])
-      session[:teacher_id] = teacher.id
-      redirect_to teacher_path(teacher.id)
+    if @teacher = Teacher.find_by(email: params[:teacher][:email])
+      @teacher && @teacher.authenticate(params[:password])
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
     else
-      #add flash message for incorrect login info
       render :new
+    end
+  end
+
+  def omniauth
+    if params[:provider] == 'google_oauth2'
+      @teacher = Teacher.create_with_google_omniauth(auth)
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
+    else
+      params[:provider] == 'facebook'
+      @teacher = Teacher.create_with_facebook_omniauth(auth)
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
     end
   end
 

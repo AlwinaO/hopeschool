12965fe0d9cc30707cd973ac1893e79c3f5afcda
AlwinaO <aoyewole2586@gmail.com>
2019-08-22 22:08:17 -0400

    add omniauth methods to be called in the omniauth controller action

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -15,7 +15,14 @@ class Teacher < ApplicationRecord
   # accepts_nested_attributes_for :semesters
   # accepts_nested_attributes_for :teacher_semesters, allow_destroy: true, reject_if: lambda {|attributes| attributes['teacher_grade'].blank?}
 
-  def self.create_with_omniauth(auth)
+  def self.create_with_google_omniauth(auth)
+    find_or_create_by(email: auth[:info][:email]) do |t|
+      t.name = auth.info.name
+      t.password = SecureRandom.hex
+    end
+  end
+
+  def self.create_with_facebook_omniauth(auth)
     teacher = find_or_create_by(uid: auth['uid'], provider: auth['provider'])
        teacher.name = auth['info']['name']
        teacher.email = auth['info']['email']

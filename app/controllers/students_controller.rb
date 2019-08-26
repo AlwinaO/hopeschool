00fdea0e2a7359e3d2931e79195419f2956e43ba
AlwinaO <aoyewole2586@gmail.com>
2019-08-26 14:04:00 -0400

    add before_action/private methods to set_teacher/set_student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,8 @@
 class StudentsController < ApplicationController
 
+  before_action :set_teacher, only:[:new, :edit, :update, :destroy]
+  before_action :set_student, only:[:show, :edit, :update, :destroy]
+
   def index
     @students = Student.all
   end
@@ -8,9 +11,8 @@ class StudentsController < ApplicationController
     # check if the student is nested and it has the proper teacher id
     # binding.pry
     # when adding the classroom_id no need to build the form for the classroom; just whitelist the :classroom_id in the strong params
-    @teacher = Teacher.find_by_id(params[:teacher_id])
     if (params[:teacher_id] && @teacher)
-      @student = @teacher.students.build
+      @student = current_teacher.students.build
     else
       @student = Student.new
       @student.build_teacher #belongs_to
@@ -21,7 +23,7 @@ class StudentsController < ApplicationController
 
     @student = current_teacher.students.build(student_params)
     if @student.save
-      # binding.pry
+      flash[:notice] = "Student profile has been successfully created."
       redirect_to student_path(@student)
     else
       @student.build_teacher unless @student.teacher
@@ -29,23 +31,17 @@ class StudentsController < ApplicationController
     end
   end
 
-
   def show
-    @student = Student.find(params[:id])
   end
 
-  # create a private method to set the student by finding the current_teacher and student for the edit, update, and destroy methods
   def edit
-    current_teacher = Teacher.find_by_id(params[:teacher_id])
-    @student = Student.find_by_id(params[:id])
-    # binding.pry
+    redirect_if_not_authorized_to_edit(@student)
   end
 
   def update
-    current_teacher = Teacher.find_by_id(params[:teacher_id])
-    @student = Student.find_by_id(params[:id])
-    # binding.pry
+    redirect_if_not_authorized_to_edit(@student)
     if @student.update(student_params)
+      flash[:notice] = "Student profile has been successfully updated."
       redirect_to student_path(@student)
     else
       render :edit
@@ -53,8 +49,7 @@ class StudentsController < ApplicationController
   end
 
   def destroy
-    current_teacher = Teacher.find_by_id(params[:teacher_id])
-    @student = Student.find_by_id(params[:id])
+    redirect_if_not_authorized_to_edit(@student)
     @student.destroy
     redirect_to teacher_students_path
   end
@@ -62,5 +57,16 @@ class StudentsController < ApplicationController
   private
     def student_params
       params.require(:student).permit(:name, :grade, :score, :teacher_id, :classroom_id)
+    end
+
+    # create a private methods to set the student by finding the current_teacher and student for the edit, update, and destroy methods
+    def set_teacher
+      current_teacher = Teacher.find_by_id(params[:teacher_id])
+      redirect_to root_path if !current_teacher
+    end
+
+    def set_student
+      @student = Student.find_by_id(params[:id])
+      redirect_to teacher_path(current_teacher) if !@student
     end
 end

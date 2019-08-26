4f4ec7294c7fbd3a4cb420698cb8cfd337d32e03
AlwinaO <aoyewole2586@gmail.com>
2019-08-26 14:02:52 -0400

    add flash messages & private method to set_session_and_redirect to teacher

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,23 +13,22 @@ class SessionsController < ApplicationController
   def create
     if @teacher = Teacher.find_by(email: params[:teacher][:email])
       @teacher && @teacher.authenticate(params[:password])
-      session[:teacher_id] = @teacher.id
-      redirect_to teacher_path(@teacher)
+      set_session_and_redirect
     else
+      flash.now[:error] = "You were not logged in. Please try again."
       render :new
     end
   end
 
+  # login with Google or Facebook OAuth
   def omniauth
     if params[:provider] == 'google_oauth2'
       @teacher = Teacher.create_with_google_omniauth(auth)
-      session[:teacher_id] = @teacher.id
-      redirect_to teacher_path(@teacher)
+      set_session_and_redirect
     else
       params[:provider] == 'facebook'
       @teacher = Teacher.create_with_facebook_omniauth(auth)
-      session[:teacher_id] = @teacher.id
-      redirect_to teacher_path(@teacher)
+      set_session_and_redirect
     end
   end
 
@@ -37,12 +36,19 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:teacher_id)
     current_teacher = nil
+    flash[:notice] = "You have successfully logged out."
     redirect_to root_path
   end
 
   private
 
+  # sets the request hash for the omniauth values received
     def auth
       request.env['omniauth.auth']
+    end
+
+    def set_session_and_redirect
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
     end
 end

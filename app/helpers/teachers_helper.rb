88d472045dd765860659adbf48b0cfd8f16f613f
AlwinaO <aoyewole2586@gmail.com>
2019-08-26 15:03:03 -0400

    create view helper to display grade format for the teacher's semester

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,2 +1,10 @@
 module TeachersHelper
+
+  def display_grade_format(teacher)
+    if teacher.teacher_semesters.exists?
+      content_tag(:h4, teacher.teacher_semesters.grade_format)
+    else
+      content_tag(:h4, "Teacher Grade not available, please click below to add a grade.")
+    end
+  end
 end

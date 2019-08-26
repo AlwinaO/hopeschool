a305ae62619bdd4db77689d86f7e56fd9dd5c724
AlwinaO <aoyewole2586@gmail.com>
2019-08-26 14:00:57 -0400

    add require_login & authorized_to_edit methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 
-
     def current_teacher
       @current_teacher ||= Teacher.find_by(id: session[:teacher_id]) if session[:teacher_id]
     end
@@ -9,9 +8,21 @@ class ApplicationController < ActionController::Base
       !!current_teacher
     end
 
-    def authenticate_teacher
-      if !logged_in?
+    def require_login
+      unless logged_in?
+        flash[:error] = "You must be logged in to access this section."
         redirect_to root_path
+      end
+    end
+
+    def authorized_to_edit?(student)
+      student.teacher == current_teacher
+    end
+
+    def redirect_if_not_authorized_to_edit(student)
+      if !authorized_to_edit?(student)
+        flash[:error] = "You must be the teacher to edit this student."
+        redirect_to teacher_path(current_teacher)
       end
     end
 end
